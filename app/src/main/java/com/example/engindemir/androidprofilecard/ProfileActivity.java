fa9b0401f1d6b1a9b534c60d86e4c0a6c4fcbc95
engindmr7 <engindmr7@gmail.com>
2017-10-16 12:51:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/engindemir/androidprofilecard/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.example.engindemir.androidprofilecard;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by engindemir on 22.04.2017.
+ */
+
+public class ProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.profile_activity);
+    }
+}
